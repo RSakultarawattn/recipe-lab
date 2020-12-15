31350e3860fb6e5ebfc7452d5a998a9c70332131
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-12-15 14:34:20 -0800

    logs table

--- sql/setup.sql
+++ sql/setup.sql
@@ -5,3 +5,11 @@ CREATE TABLE recipes (
   name TEXT NOT NULL,
   directions TEXT[]
 );
+
+CREATE TABLE logs {
+  id BIGINT   GENERATED ALWAYS AS IDENTITY,
+  date_of_event TEXT NOT NULL,
+  notes VARCHAR(20),
+  rating INT BETWEEN(0 and 10),
+  recipe_id BIGINT NOT NULL REFERENCES recipes(id)
+};
